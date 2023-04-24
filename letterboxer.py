a543d48cdd482d2ec6745cf546860e92dd31d279
brendan.craven <brendan.craven@student.howest.be>
2023-04-24 23:30:30 +0200

    fix letterboxing functionality, write new image to file

--- letterboxer.py
+++ letterboxer.py
@@ -1,19 +1,42 @@
-import numpy as np
 import cv2
+import numpy as np
+import os
+import sys
 
-image = cv2.imread("screencap.png")
+
+def add_letterboxed(path):
+    directory, filename = os.path.split(path)
+    name, ext = os.path.splitext(filename)
+    new_name = name + '_letterboxed' + ext
+    return os.path.join(directory, new_name)
+
+
+if len(sys.argv) < 2:
+    print("Usage: python letterboxer.py <path to image>")
+    sys.exit(1)
+
+if not isinstance(sys.argv[1], str):
+    print("Error: <path to image> must be a string")
+    sys.exit(1)
+
+image_path = str(sys.argv[1])
+image_name = os.path.basename(image_path)
+
+image = cv2.imread(image_path)
 image_height, image_width = image.shape[:2]
 
 black_image = np.zeros((image_height, image_width, 3), np.uint8)
 
-frame_ratio = 0.90
+frame_ratio = float(sys.argv[2]) / 2 if len(sys.argv) == 3 else 0.1 / 2
 
-lower_box = int(image_height - (image_height * frame_ratio))
-upper_box = int(image_height * frame_ratio)
-black_image[0:lower_box, 0:image_width] = 255
-black_image[upper_box:image_height, 0:image_width] = 255
+upper_box = int(image_height - (image_height * frame_ratio))
+lower_box = int(image_height * frame_ratio)
+black_image[0:lower_box, :] = 255
+black_image[upper_box:image_height, :] = 255
 
 final_image = cv2.subtract(image, black_image)
-
-cv2.imshow('result', final_image), cv2.waitKey(0)
+cv2.imshow("Final result", final_image)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+cv2.imwrite(add_letterboxed(image_path), final_image)
